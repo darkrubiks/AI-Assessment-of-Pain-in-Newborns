b8c472b16f7bdbae206e4f292ca9200f7fd9b5be
darkrubiks <leonardoantunes123@gmail.com>
2023-01-03 11:47:01 -0300

    Added data_augmentation.py and utils.py
    * utils.py contains useful functions
    * data_augmentation.py uses Albumentations to generate images from each fold

---
+++ data_augmentation.py
@@ -0,0 +1,86 @@
+"""
+data_augmentation.py
+
+Author: Leonardo Antunes Ferreira
+Date: 03/01/2023
+
+This code is responsible for augmenting the train set. The Data Augmentation
+pipeline will generate 20 new images from 1 single face image. The facial land-
+marks are also augmented.
+"""
+import os
+import pickle
+import cv2
+import albumentations as A
+import pandas as pd
+from tqdm import tqdm
+from ast import literal_eval
+from utils import *
+
+
+# Augmentation Pipeline, Affine transformation is always applied, the Horizontal
+# Flip and RandomBrightnessContrast are applied with a 50% chance
+transform = A.Compose([
+
+        A.Affine(scale=(0.70, 1.5),
+                 translate_percent=(-0.2,0.2),
+                 rotate=(-30, 30),
+                 shear=(-10, 10),
+                 mode=cv2.BORDER_REPLICATE,
+                 p=1),
+
+        A.RandomBrightnessContrast(brightness_limit=0.2,
+                                   contrast_limit=0.2,
+                                   p=0.5),
+
+        A.HorizontalFlip(p=0.5),
+    ],
+
+    keypoint_params=A.KeypointParams(format='xy'))
+
+# Read the data from the .csv
+iCOPE_UNIFESP_data = pd.read_csv('iCOPE+UNIFESP_data.csv')
+iCOPE_UNIFESP_data['face_coordinates'] = iCOPE_UNIFESP_data['face_coordinates'].apply(lambda x: literal_eval(x))
+iCOPE_UNIFESP_data['landmarks_coordinates'] = iCOPE_UNIFESP_data['landmarks_coordinates'].apply(lambda x: literal_eval(x))
+
+folds_path = 'Datasets\\Folds'
+
+# For each Fold the images are augmented 20 times, verifying that the landmarks
+# are still in bounds of the new image
+for fold in range(10):
+    print(f'Augmenting Fold: {fold:02}')
+
+    fold = str(fold)
+
+    train_fold_path = os.path.join(folds_path, fold, 'Train')
+
+    for file_name in tqdm(os.listdir(train_fold_path)):
+        # Read image
+        img = cv2.imread(os.path.join(train_fold_path, file_name))
+        # Get correspondent Face and Landmarks coordinates
+        face_coords = iCOPE_UNIFESP_data[iCOPE_UNIFESP_data['new_file_name']==file_name]['face_coordinates'].values[0]
+        landmarks_coords = iCOPE_UNIFESP_data[iCOPE_UNIFESP_data['new_file_name']==file_name]['landmarks_coordinates'].values[0]
+        # Scale the landmarks to the cropped face
+        scaled_landmarks = [scale_coords(x, y, face_coords[0], face_coords[1]) for x, y in landmarks_coords]
+
+        for i in range(20):
+
+            transformed = transform(image=img, keypoints=scaled_landmarks)
+            
+            # Keep generating images until all landmarks are present
+            while len(transformed['keypoints']) < 5:
+                transformed = transform(image=img, keypoints=scaled_landmarks)
+
+            # Save image
+            aug_file_name = f'{i:02}_AUG_{file_name}'
+            cv2.imwrite(os.path.join(folds_path, fold, 'Train', aug_file_name), transformed['image'])
+            # Save landmarks
+            auf_landmarks_file = open(os.path.join(folds_path, fold, 'Train', aug_file_name.split('.jpg')[0]), 'wb')
+            pickle.dump(transformed['keypoints'], auf_landmarks_file)
+            auf_landmarks_file.close()
+
+
+
+
+
+    
---
+++ utils.py
@@ -0,0 +1,23 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+def vis_keypoints(image, keypoints, color=(0,255,0), diameter=15):
+
+    image = image.copy()
+
+    for (x, y) in keypoints:
+        cv2.circle(image, (int(x), int(y)), diameter, color, -1)
+        
+    plt.figure(figsize=(8, 8))
+    plt.axis('off')
+    plt.imshow(image)
+    plt.show()
+
+def scale_coords(x, y, x_new_origin, y_new_origin):
+
+    scaled_x = x - x_new_origin
+    scaled_y = y - y_new_origin
+
+    return scaled_x, scaled_y
+
