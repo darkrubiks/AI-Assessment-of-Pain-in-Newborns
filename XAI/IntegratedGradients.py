abe159017874996c41c034ff41fb115ea87fe6cd
darkrubiks <leonardoantunes123@gmail.com>
2023-02-20 21:13:18 -0300

    updated IntegratedGradients methods to private

--- XAI/IntegratedGradients.py
+++ XAI/IntegratedGradients.py
@@ -23,7 +23,7 @@ class IntegratedGradients:
         self.model = model.eval().to(self.device)
         self.n_steps = n_steps
         
-    def interpolate_images(self, baseline, image):
+    def __interpolate_image(self, baseline, image):
         """
         Generates a linear interpolation between the baseline and the original
         image.
@@ -40,7 +40,7 @@ class IntegratedGradients:
 
         return interpoleted
 
-    def compute_gradients(self, image, target_class):
+    def __compute_gradients(self, image, target_class):
         """
         Calculates gradients in order to measure the relationship between changes
         to a feature and changes in the model's predictions.
@@ -52,7 +52,7 @@ class IntegratedGradients:
 
         return image.grad
 
-    def integral_approximation(self, gradients):
+    def __integral_approximation(self, gradients):
         """
         Computes the numerical approximation of an integral for Integrated
         Gradients using the Riemann Trapezoidal rule.
@@ -67,48 +67,14 @@ class IntegratedGradients:
         Generates the Integrated Gradients feature attributions.
         """
         image = image.to(self.device)
-        baseline = baseline = baseline.to(self.device)
+        baseline = baseline.to(self.device)
 
-        interpoleted = self.interpolate_images(baseline, image)
-        gradients = self.compute_gradients(interpoleted, target_class)
-        integrated_gradients = self.integral_approximation(gradients)
+        interpoleted = self.__interpolate_image(baseline, image)
+        gradients = self.__compute_gradients(interpoleted, target_class)
+        integrated_gradients = self.__integral_approximation(gradients)
         
         attribution = (image - baseline) * integrated_gradients
 
-        heatmap = torch.abs(attribution).sum(0)
+        heatmap = torch.abs(attribution.sum(dim=0))
 
         return heatmap.detach().cpu()
-    
-if __name__ == '__main__':
-    
-    import cv2
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import torch
-    from torchvision import transforms
-
-    transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.36703529, 0.41083294, 0.50661294], std=[1, 1, 1])
-        ])
-    
-    path = '/content/Mestrado/Datasets/Folds/9/Test/ID280_COPE_S25_no_pain.jpg'
-    target_class = 0
-    image = cv2.imread(path) # Load BGR image to VGGFace 
-    image = image/255 # Normalize to [0-1]
-    image = np.float32(image)
-    image = transform(image)
-
-    baseline = torch.zeros((3,224,224)) # Baseline image its all black pixels
-
-    model = torch.load('/content/Mestrado/models/best_model.pt') # Load your model
-
-    ig = IntegratedGradients(model, 50)
-
-    heatmap = ig.attribution_mask(baseline, image, target_class=0)
-
-    plt.imshow(heatmap)
-
-
-
