0577932dcc682b73ad67260a597468b16a112b89
darkrubiks <leonardoantunes123@gmail.com>
2023-03-06 20:53:35 -0300

    minor fixes

--- models/NCNN.py
+++ models/NCNN.py
@@ -8,7 +8,8 @@ This file contains the NCNN model implemented by:
 
 G. Zamzmi, R. Paul, D. Goldgof, R. Kasturi and Y. Sun on 
 
-"Pain Assessment From Facial Expression: Neonatal Convolutional Neural Network (N-CNN)"
+"Pain Assessment From Facial Expression: Neonatal Convolutional Neural Network
+(N-CNN)"
 
 doi: https://doi.org/10.1109/IJCNN.2019.8851879
 """
@@ -23,20 +24,20 @@ class NCNN(nn.Module):
         super(NCNN, self).__init__()
 
         # Branch 1
-        self.maxpool_1_1 = nn.MaxPool2d(kernel_size=10, stride=10, padding=0)
+        self.maxpool_1_1 = nn.MaxPool2d(10, 10, 0)
         # Branch 2
-        self.conv_2_1 = nn.Conv2d(3, 64, 5, 1, padding=2)
-        self.maxpool_2_1 = nn.MaxPool2d(kernel_size=3, stride=3, padding=1)
-        self.conv_2_2 = nn.Conv2d(64, 64, 5, 1, padding=2)
-        self.maxpool_2_2 = nn.MaxPool2d(kernel_size=3, stride=3, padding=1)
+        self.conv_2_1 = nn.Conv2d(3, 64, 5, 1, 2)
+        self.maxpool_2_1 = nn.MaxPool2d(3, 3, 1)
+        self.conv_2_2 = nn.Conv2d(64, 64, 5, 1, 2)
+        self.maxpool_2_2 = nn.MaxPool2d(3, 3, 1)
         self.dropout_2 = nn.Dropout(0.1)
         # Branch 3
-        self.conv_3_1 = nn.Conv2d(3, 64, 5, 1, padding=2)
-        self.maxpool_3_1 = nn.MaxPool2d(10 ,10, padding=0)
+        self.conv_3_1 = nn.Conv2d(3, 64, 5, 1, 2)
+        self.maxpool_3_1 = nn.MaxPool2d(10 ,10, 0)
         self.dropout_3 = nn.Dropout(0.1)
         # Merge Branch
-        self.conv_4 = nn.Conv2d(64 + 64 + 3, 64, 2, 1, padding=1, dilation=2)
-        self.maxpool_4 = nn.MaxPool2d(2, 2, padding=0)
+        self.conv_4 = nn.Conv2d(64 + 64 + 3, 64, 2, 1, 1, 2)
+        self.maxpool_4 = nn.MaxPool2d(2, 2, 0)
         self.fc_4 = nn.Linear(6 * 6 * 64, 8)
         self.dropout_4 = nn.Dropout(0.1)
         self.output = nn.Linear(8, 2)
