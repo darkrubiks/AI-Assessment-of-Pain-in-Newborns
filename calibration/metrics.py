e7cf86177d446a93b16e171298af33f48d59b85a
darkrubiks <leonardoantunes123@gmail.com>
2023-07-18 20:57:11 -0300

    New-calibration-metrics (#18)

--- calibration/metrics.py
+++ calibration/metrics.py
@@ -7,6 +7,7 @@ Date: 06/05/2023
 This file contains metrics that can be used to validate the model's calibration.
 """
 import numpy as np
+from sklearn.metrics import log_loss, brier_score_loss
 
 
 def ECE(confs: np.ndarray,
@@ -61,7 +62,7 @@ def MCE(confs: np.ndarray,
     ----------
     confs : confidence on the positive class
 
-    labels : true labels as binary targetes
+    labels : true labels as binary targets
 
     n_bins : number of bins to discretize
 
@@ -92,3 +93,92 @@ def MCE(confs: np.ndarray,
             mce.append(np.abs(avg_confidence_in_bin - fraction_of_postives_in_bin))
 
     return max(mce)
+
+def negative_log_likelihood(confs: np.ndarray,
+                            labels: np.ndarray) -> np.float32:
+    """
+    Calculates the Negative Log Likelihood.
+
+    Parameters
+    ----------
+    confs : confidence on the positive class
+
+    labels : true labels as binary targets
+
+    Returns
+    -------
+    nll : the Negative Log Likelihood
+
+    See Also
+    --------
+    scikitlearn : https://scikit-learn.org/stable/modules/generated/sklearn.metrics.log_loss.html
+    """
+    nll = log_loss(labels, confs)
+
+    return nll
+
+def brier_score(confs: np.ndarray, 
+                labels: np.ndarray) -> np.float32:
+    """
+    Calculates the Brier Score.
+
+    Parameters
+    ----------
+    confs : confidence on the positive class
+
+    labels : true labels as binary targets
+
+    Returns
+    -------
+    brier : the Negative Log Likelihood
+
+    See Also
+    --------
+    scikitlearn : https://scikit-learn.org/stable/modules/generated/sklearn.metrics.brier_score_loss.html
+    """
+    brier = brier_score_loss(labels, confs)
+
+    return brier
+
+def calibration_curve(confs: np.ndarray,
+                      labels: np.ndarray,
+                      n_bins: int=10) -> np.ndarray:
+    """
+    Compute true and predicted probabilities for a calibration curve.
+
+    Adapted from scikitlearn to return the amount of positive class 
+    samples in each bin.
+
+    Parameters
+    ----------
+    confs : confidence on the positive class
+
+    labels : true labels as binary targets
+
+    n_bins : number of bins to discretize
+
+    Returns
+    -------
+    prob_true : the proportion of samples whose class is the positive class,
+    in eachbin (fraction of positives)
+
+    prob_pred : the mean predicted probability in each bin
+
+    bin_true : the amount of positive class samples in each bin
+
+    See Also
+    --------
+    scikitlearn : https://scikit-learn.org/stable/modules/generated/sklearn.calibration.calibration_curve.html
+    """
+    bins = np.linspace(0.0, 1.0, n_bins + 1)
+    binids = np.searchsorted(bins[1:-1], confs)
+
+    bin_sums = np.bincount(binids, weights=confs, minlength=len(bins))
+    bin_true = np.bincount(binids, weights=labels, minlength=len(bins))
+    bin_total = np.bincount(binids, minlength=len(bins))
+
+    nonzero = bin_total != 0
+    prob_true = bin_true[nonzero] / bin_total[nonzero]
+    prob_pred = bin_sums[nonzero] / bin_total[nonzero]
+    
+    return prob_true, prob_pred, bin_true
