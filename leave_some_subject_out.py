22158c26f246d60bdfbb4e37bc98dfed8111d3c4
darkrubiks <leonardoantunes123@gmail.com>
2023-08-03 16:04:19 -0300

    major refactor in dataset creation process

--- leave_some_subject_out.py
+++ leave_some_subject_out.py
@@ -1,38 +1,34 @@
-"""
-leave_some_subject_out.py
-
-Author: Gabriel de Almeida Sá Coutrin and Leonardo Antunes Ferreira
-Date: 19/12/2022
-
-This code is responsible for creating the folds for training and testing. The
-folds are created based on the leave some subject out method developed during
-Coutrin's Masters Degree, ensuring the same number of UNIFESP and COPE images
-are used during training and testing, and also preventing subject leakage from
-training to testing set. Only pain and no pain classifications are used. Also,
-it creates a held-out-calibration set for further evaluation.
-"""
 import os
 from shutil import copyfile, rmtree
 
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold, train_test_split
-from tqdm import tqdm
+
+from utils.utils import create_folder
 
 # Constants
-FOLDS_FOLDER_PATH = os.path.join('Datasets','Folds')
-DATASETS_FOLDER_PATH = os.path.join('Datasets','DatasetFaces')
-CALIBRATION_FOLDER_PATH = os.path.join('Datasets','Calibration')
+FOLDS_FOLDER_PATH = os.path.join('Datasets', 'Folds')
+DATASETS_FOLDER_PATH = os.path.join('Datasets', 'DatasetFaces')
+CALIBRATION_FOLDER_PATH = os.path.join('Datasets', 'Calibration')
 CALIBRATION_SIZE = 0.1
-N_FOLDS = 10
+N_FOLDS = 5
 
-# Remove folder if present
+def copy_files(src_files, dst_folder):
+    """
+    Tries to create a folder on the informed path.
+    """
+    for src_file in src_files:
+        src = os.path.join(DATASETS_FOLDER_PATH, 'Images', src_file)
+        dst = os.path.join(dst_folder, src_file)
+        copyfile(src, dst)
+
 if os.path.exists(FOLDS_FOLDER_PATH):
     rmtree(FOLDS_FOLDER_PATH)
-os.mkdir(FOLDS_FOLDER_PATH)
+create_folder(FOLDS_FOLDER_PATH)
 
 if os.path.exists(CALIBRATION_FOLDER_PATH):
     rmtree(CALIBRATION_FOLDER_PATH)
-os.mkdir(CALIBRATION_FOLDER_PATH)
+create_folder(CALIBRATION_FOLDER_PATH)
 
 # Read the data
 dataframe = pd.read_csv('iCOPE+UNIFESP_data.csv')
@@ -43,64 +39,46 @@ dataframe = dataframe[(dataframe['class']=='pain') |
 
 # Split the data into calibration and training/testing
 _, X_calib, _, y_calib = train_test_split(
-    dataframe["new_file_name"], 
-    dataframe["class"], 
-    test_size=CALIBRATION_SIZE, 
-    random_state=42, 
+    dataframe["new_file_name"],
+    dataframe["class"],
+    test_size=CALIBRATION_SIZE,
+    random_state=42,
     stratify=dataframe["class"]
 )
 
 # Copy the calibration images to a separate folder
-for file_name in X_calib:
-    src = os.path.join(DATASETS_FOLDER_PATH, 'Images', file_name)
-    dst = os.path.join(CALIBRATION_FOLDER_PATH, file_name)
-    copyfile(src, dst)
+copy_files(X_calib, CALIBRATION_FOLDER_PATH)
 
 # Drop the calibration images from the training/testing data
 dataframe = dataframe.drop(y_calib.index)
 
 # Gather unique subjects
-dataframe_no_duplicates = dataframe.drop_duplicates(subset=['new_subject'])
-
-subjects = dataframe_no_duplicates['new_subject'].values
-datasets = dataframe_no_duplicates['dataset'].values
+unique_subjects = list(set(dataframe['new_subject']))
+datasets = dataframe.drop_duplicates(subset=['new_subject'])['dataset'].values
 
 train_subjects = []
 test_subjects = []
 
-# The StratifiedKFold is used to split the data between subjects but also consi-
-# dering their original datasets, achieving balance between datasets
+# The StratifiedKFold is used to split the data between subjects but also consider their original datasets, achieving balance between datasets
 skf = StratifiedKFold(n_splits=N_FOLDS)
 
-for train_index, test_index in skf.split(subjects, datasets):
-    train_subjects.append(subjects[train_index])
-    test_subjects.append(subjects[test_index])
+for train_index, test_index in skf.split(unique_subjects, datasets):
+    train_subjects.append([unique_subjects[i] for i in train_index])
+    test_subjects.append([unique_subjects[i] for i in test_index])
 
 for fold in range(N_FOLDS):
     # Create the fold folder
     fold_path = os.path.join(FOLDS_FOLDER_PATH, str(fold))
-    os.mkdir(fold_path)
+    create_folder(fold_path)
 
     # Create the train and test folders
     train_path = os.path.join(fold_path, "Train")
-    os.mkdir(train_path)
+    create_folder(train_path)
     test_path = os.path.join(fold_path, "Test")
-    os.mkdir(test_path)
+    create_folder(test_path)
 
-    print('Copying Train Subjects')
-    for train_subject in tqdm(train_subjects[int(fold)]):
-        file_names = dataframe[dataframe['new_subject']==train_subject]['new_file_name']
-        
-        for file_name in file_names:
-            src = os.path.join(DATASETS_FOLDER_PATH, 'Images', file_name)
-            dst = os.path.join(train_path, file_name)
-            copyfile(src, dst)
+    # Copy Train Subjects
+    copy_files(dataframe[dataframe['new_subject'].isin(train_subjects[fold])]['new_file_name'], train_path)
 
-    print('Copying Test Subjects')
-    for test_subject in tqdm(test_subjects[int(fold)]):
-        file_names = dataframe[dataframe['new_subject']==test_subject]['new_file_name']
-        
-        for file_name in file_names:
-            src = os.path.join(DATASETS_FOLDER_PATH, 'Images', file_name)
-            dst = os.path.join(test_path, file_name)
-            copyfile(src, dst)
+    # Copy Test Subjects
+    copy_files(dataframe[dataframe['new_subject'].isin(test_subjects[fold])]['new_file_name'], test_path)
