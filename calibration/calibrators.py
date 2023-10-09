69eee167dec523f5149918b75993aa4dc28823d6
darkrubiks <leonardoantunes123@gmail.com>
2023-10-09 17:09:48 -0300

    added docs

--- calibration/calibrators.py
+++ calibration/calibrators.py
@@ -297,21 +297,50 @@ class HistogramBinning:
 
 
 class MCDropout:
+    """
+    Monte-Carlo Dropout is one of the most famous way to model uncertainty in Deep-Learning.
+    Considering that a Deep-Learning model was trained with Dropout layers, we can leverage
+    this implementation during test phase to generate different outputs/predictions scores
+    that tries to aproximate the underlying uncertainty of the input data.
+
+    See Also
+    --------
+    Gal, Yarin, and Zoubin Ghahramani. "Dropout as a bayesian approximation: Representing model 
+    uncertainty in deep learning." international conference on machine learning. PMLR, 2016.
+    """
+
     def __init__(self, model, p) -> None:
         self.model = model
         self.p = p
 
     def _enable_dropout(self):
+        """
+        Enables the dropout layers during inference
+        """
         for m in self.model.modules():
             if m.__class__.__name__.startswith('Dropout'):
                 m.p = self.p
                 m.train()
 
-    def predict(self, x: np.ndarray, r) -> np.ndarray:
-        self._enable_dropout()
-        preds = np.zeros(r)
+    def predict(self, x: np.ndarray, reps: int=10) -> np.ndarray:
+        """
+        The predict method will auto enable the dropout layers of the model,
+        returning all the predictions made for the given repetition value.
 
-        for i in range(r):
+        Parameters
+        ----------
+        x : the input of the model
+
+        reps : the number of repetitions to generate predictions scores
+
+        Parameters
+        ----------
+        All predictions scores in a numpy array
+        """
+        self._enable_dropout()
+        preds = np.zeros(reps)
+
+        for i in range(reps):
             preds[i] = self.model.predict(x).detach().cpu().numpy()
 
         return preds
