0a650a84fe83fce303a2b58e47c12bf9783cde83
darkrubiks <leonardoantunes123@gmail.com>
2023-11-05 12:51:21 -0300

    XAI-score (#29)

--- XAI/__init__.py
+++ XAI/__init__.py
@@ -2,3 +2,4 @@ from XAI.attribution_mask_processing import attribution_mask_processing
 from XAI.IntegratedGradients import IntegratedGradients
 from XAI.GradCAM import GradCAM
 from XAI.FeatureMaps import FeatureMaps
+from XAI.metrics import *
