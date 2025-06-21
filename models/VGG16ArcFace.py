1f01bb1f163c89df7813665715ec9f4047b1b757
darkrubiks <leonardoantunes123@gmail.com>
2025-06-21 19:52:21 -0300

    Replace vgg16_bn with vgg16 in VGG16ArcFace model initialization

--- models/VGG16ArcFace.py
+++ models/VGG16ArcFace.py
@@ -1,15 +1,16 @@
 import torch
-from torchvision.models import vgg16_bn
+from torchvision.models import vgg16
 import torch.nn as nn
 
 class VGG16ArcFace(nn.Module):
     def __init__(self, num_classes: int = 512, dropout: float = 0.5) -> None:
         super(VGG16ArcFace, self).__init__()
-        self.vgg = vgg16_bn(dropout=dropout)
+        self.vgg = vgg16(dropout=dropout)
         self.vgg.classifier = nn.Identity()
         self.vgg.avgpool = nn.Identity()
         self.embeddings = nn.Sequential(
             nn.Flatten(),
+            nn.Dropout(dropout),
             nn.Linear(512 * 7 * 7, num_classes, bias=False),
             nn.BatchNorm1d(num_classes)
         )
