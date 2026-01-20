a36a7daa670c14211fee9b8798f33b402a746555
darkrubiks <leonardoantunes123@gmail.com>
2026-01-20 17:58:13 -0300

    updates

--- dataloaders/presets.py
+++ dataloaders/presets.py
@@ -74,13 +74,13 @@ class PresetTransform:
         if self.model_name == 'NCNN':
             self.transforms = transforms.Compose([
                 transforms.Resize((120,120), interpolation=interpolation, antialias=True),
-                #transforms.PILToTensor(),
+                transforms.PILToTensor(),
                 transforms.ConvertImageDtype(torch.float),
             ])
         elif self.model_name == 'VGGFACE':
             self.transforms = transforms.Compose([
                 transforms.Resize((224,224), interpolation=interpolation, antialias=True),
-                #transforms.PILToTensor(),
+                transforms.PILToTensor(),
                 transforms.ConvertImageDtype(torch.float),
                 transforms.Lambda(lambda x: x[[2, 1, 0], ...]),
                 # VGGFace normalization
@@ -90,7 +90,7 @@ class PresetTransform:
         elif self.model_name == 'VIT':
             self.transforms = transforms.Compose([
                 transforms.Resize((224,224), interpolation=interpolation, antialias=True),
-                #transforms.PILToTensor(),
+                transforms.PILToTensor(),
                 transforms.ConvertImageDtype(torch.float),
                 # Imagenet normalization
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
